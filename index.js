79fa9fd30a8a73507efe1cf7cf37729929eab5ea
unknown <muktinathrajbanshi786@gmail.com>
2026-01-22 20:35:07 +0545

    hait khatra mane danger

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ const decreaseNumber = () => {
         itemval.value = parseInt(itemval.value) - 1;
     }
 }
+
+const increaseNumber = () => {
+    let itemval = document.getElementById("textbox");
+    console.log(itemval);
+
+    if(itemval.value >= 5) {
+        itemval.value = 5;
+        alert("max 5 allowed");
+        itemval.style.background = "red";
+        itemval.style.color = "#fff";
+    } else {
+        itemval.value = parseInt(itemval.value) + 1;
+    }
+}
