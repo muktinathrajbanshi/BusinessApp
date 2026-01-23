736347f43178699634070fcfb30db561ae3d49aa
unknown <muktinathrajbanshi786@gmail.com>
2026-01-23 07:10:26 +0545

    Happy saraswati puja 2082 and very very Good Morning

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const decreaseNumber = () => {
-    let itemval = document.getElementById("textbox");
+const decreaseNumber = (incdec) => {
+    let itemval = document.getElementById(incdec);
     console.log(itemval);
 
     if(itemval.value <= 0) {
         itemval.value = 0;
+        alert("Negative quantity not allowed");
 
     } else {
         itemval.value = parseInt(itemval.value) - 1;
-        alert("Negative quantity not allowed");
          itemval.style.background = "#fff";
         itemval.style.color = "#000";
     }
 }
 
-const increaseNumber = () => {
-    let itemval = document.getElementById("textbox");
+const increaseNumber = (incdec) => {
+    let itemval = document.getElementById(incdec);
     console.log(itemval);
 
     if(itemval.value >= 5) {
